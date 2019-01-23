0c18552cdaa43b1e59d9be51441a6b8d7e3dd391
Heather Kirk <hskirk@gmail.com>
2019-01-23 23:57:55 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(languages)
   i = 0 
   speech = []
   while i < languages.length
-  speech.upcase do |languages|
+  my_collect(languages) do |language|
   speech<< yield(languages)
   i +=1 
 end
